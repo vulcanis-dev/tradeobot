dae707a92b87f1cc47a4b68850c1ab4b47c884c5
vulcanis-dev <silverstriker49@gmail.com>
2021-09-16 15:09:54 +0100

    test unsoldBuys

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var tradeEngine = require('./tradeEngine.js');
 
+//returns buys with no matching sell(has to round to 4 decimal places to account for fill discrepencies)
+var unsoldBuys = tradeEngine.getUnsoldBuys().then(response => {
+    console.log(response);
+});
+
+//look to see if anymore fills have been completed 
+// if no continue with previous order
+// if yes then refresh unsold buys list
+//call calculate difference on all unmatched buys
+
+console.log(unsoldBuys);
 //execute function every # of milliseconds
-setInterval(tradeEngine.calculateDifference, 5000);
+//setInterval(tradeEngine.calculateDifference, 5000);
 
 /* 
 tradeEngine.getAccounts('GBP').then(response => {
