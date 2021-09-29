55e7aa2097b92c8f010ce54af4fef5b4badd01e0
vulcanis-dev <silverstriker49@gmail.com>
2021-09-29 17:05:12 +0100

    ifttt notifications

--- notify.js
+++ notify.js
@@ -1,19 +1,35 @@
 var config = require('./config');
 const axios = require('axios');
 
-async function sendNotification(message) {
+async function sendTradeNotification(product, side, value) {
     var key = config.notifyKey;
-    var client = getClient(key);
-    var response = await client.get().catch(error => {
+    var client = getClient(key, "notify");
+    var body = { "value1" : product, "value2" : side, "value3" : value } 
+    var response = await client.post('', body).catch(error => {
         console.error('There was an error!', error);
     });;
 }
 
-function getClient(key) {
+async function sendNotification(message) {
+    var key = config.notifyKey;
+    var client = getClient(key, "notify coinbase event");
+    var body = { "msg": message }; 
+    var response = await client.post('', body).catch(error => {
+        console.error('There was an error!', error);
+    });;
+}
+
+function getClient(key, event) {
+    var urlPart = '';
+    if(event == 'notify') {
+        urlPart = '/with/key/';
+    } else {
+        urlPart = '/json/with/key/';
+    }
     return axios.create({
-        baseURL: 'https://maker.ifttt.com/trigger/notify/with/key/' + key,
+        baseURL: 'https://maker.ifttt.com/trigger/' + event + urlPart + key,
         timeout: 1000
     });
 }
 
-module.exports = { sendNotification };
+module.exports = { sendNotification, sendTradeNotification };
